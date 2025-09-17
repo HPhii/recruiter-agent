a6c17f6d7f319ee092b19a5cb99ce34bebeb4af9
Trinh Hieu Phi <phitrinh367@gmail.com>
2025-09-17 09:48:36 +0700

    enhance retriever ability by chunk

--- response.py
+++ response.py
@@ -29,7 +29,10 @@ def get_conversational_chain():
 def query_documents(question):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectordb = Chroma(persist_directory=CHROMA_DIR, embedding_function=embeddings)
-    retriever = vectordb.as_retriever(search_kwargs={"k": 4})
+    retriever = vectordb.as_retriever(
+        search_type="mmr",
+        search_kwargs={'k': 5, 'fetch_k': 20}
+    )
 
     llm = Ollama(model="llama3.2:1b", temperature=0.1, base_url=OLLAMA_HOST)
 
